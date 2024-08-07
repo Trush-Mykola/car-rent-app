d1302cdabe48dc688ce6f07da8d4c06bb866ba18
Mykola Trush <tryshmykola@gmail.com>
2024-08-07 15:17:15 +0300

    create project repo, add svg sprite + basic dependencies

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+// panelTruck --> Van icon
+//fullyIntegrated --> Fully Integrated icon
+//alcove --> Alcove icon
+
+function App() {
+  return (
+    <>
+      <div className="title">App</div>
+    </>
+  );
+}
+
+export default App;
